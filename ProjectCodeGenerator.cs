ef1c658b78af216145a93c117d79912bc676f189
Fabrizio <fabrizio.brioccia@hotmail.it>
2016-09-06 09:29:38 +0200

    Update ProjectCodeGenerator.cs

--- ProjectCodeGenerator.cs
+++ ProjectCodeGenerator.cs
@@ -14,8 +14,8 @@ namespace CodeGenerator
 		private IOrganizationService service;
 		private const string parentEntityName = "ava_contract";
 		private const string parentEntityLookupFieldName = "ava_contractid";
-        private const int projectCodeLevel = 2;    //the second part of an ava_code identifies the project
-        private const int projectCodePartLength = 1;
+        	private const int projectCodeLevel = 2;    //the second part of an ava_code identifies the project
+        	private const int projectCodePartLength = 1;
 
 		public ProjectCodeGenerator(EntityReference entityReference, IOrganizationService service)
 		{
@@ -36,14 +36,14 @@ namespace CodeGenerator
 			var childEntity = service.Retrieve(entityReference.LogicalName, entityReference.Id, columnSet);
 			var parentEntityReference = childEntity.GetAttributeValue<EntityReference>(parentEntityLookupFieldName);
 
-            var parentProjects = CodeGeneratorHelper.GetChildsList(service, parentEntityReference, entityReference.LogicalName, parentEntityLookupFieldName, new ColumnSet("ava_code"));
+            		var parentProjects = CodeGeneratorHelper.GetChildsList(service, parentEntityReference, entityReference.LogicalName, parentEntityLookupFieldName, new ColumnSet("ava_code"));
 
 			var projectsFromParentWithoutCurrent = parentProjects.Where(x => x.Id != entityReference.Id).ToList();
 
 			var codesAlreadyAssigned = CodeGeneratorHelper.GetCodesAlreadyAssigned(projectCodeLevel, projectsFromParentWithoutCurrent);
 
-            int partialProjectCodeToConvert = CodeGeneratorHelper.GenerateAssignableIntCodeValue(projectCodePartLength, codesAlreadyAssigned);
-            string partialProjectCode = CodeGeneratorHelper.ConvertIntToCharCode(projectCodePartLength, partialProjectCodeToConvert);
+            		int partialProjectCodeToConvert = CodeGeneratorHelper.GenerateAssignableIntCodeValue(projectCodePartLength, codesAlreadyAssigned);
+            		string partialProjectCode = CodeGeneratorHelper.ConvertIntToCharCode(projectCodePartLength, partialProjectCodeToConvert);
 
 			string projectCode = parentAvaCode + "-" + partialProjectCode;
 
